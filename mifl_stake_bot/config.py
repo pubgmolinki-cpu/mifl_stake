1fe0494eb9019fd7361586fdd84544d7e01b56ab
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-09 18:07:50 +0400

    Update config.py

--- mifl_stake_bot/config.py
+++ mifl_stake_bot/config.py
@@ -1,10 +1,6 @@
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not BOT_TOKEN or not DATABASE_URL:
-    raise ValueError("Не найден BOT_TOKEN или DATABASE_URL! Проверь файл .env или настройки хостинга.")
