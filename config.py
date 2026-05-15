522caf743470da7cb9113f05ff3decd0b20985c7
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-16 01:17:58 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Переменная окружения на Render часто начинается с postgres://
+# Но asyncpg требует именно postgresql://
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+ADMIN_IDS = [123456789, 987654321]  # Замени на свой Telegram ID
