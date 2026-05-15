d972900be8fe3ad05458584130400c328e77fe86
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-16 01:25:32 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,16 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Переменная окружения на Render часто начинается с postgres://
-# Но asyncpg требует именно postgresql://
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-ADMIN_IDS = [1866813859]  # Замени на свой Telegram ID
+ADMIN_IDS = [1866813859]  # Твой ID
+
+# --- НАСТРОЙКИ ДЛЯ RENDER (WEBHOOK) ---
+# Render автоматически выдает порт в переменную окружения PORT. Если ее нет (локально), ставим 8080
+PORT = int(os.getenv("PORT", 8080))
+
+# Ссылка на твое приложение Render (например: https://ftcl-bet.onrender.com)
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST") 
+WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
